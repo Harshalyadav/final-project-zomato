718d2f631b7987c957ce7ff9a25f1695ec2f6b37
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-10-16 23:33:16 +0530

    Create Checkout.layout.jsx

---
+++ client/src/Layout/Checkout.layout.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+// components
+import Navbar from "../Components/Navbar/checkoutNavbar";
+// redux action
+import { getCart } from "../Redux/Reducer/Cart/Cart.action";
+const CheckoutLayout = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCart());
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto px-4 lg:px-20 ">{props.children}</div>
+    </>
+  );
+};
+export default CheckoutLayout;
