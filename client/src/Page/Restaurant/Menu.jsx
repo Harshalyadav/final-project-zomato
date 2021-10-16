a121185c4f69311baa000b5a71015084e0e91981
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-10-16 23:35:24 +0530

    Create Menu.jsx

---
+++ client/src/Page/Restaurant/Menu.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+ import { useSelector, useDispatch } from "react-redux";
+
+// component
+import MenuCollection from "../../Components/restaurant/MenuCollection";
+
+// // redux actions
+ import { getImage } from "../../Redux/Reducer/Image/Image.action";
+
+const Menu = () => {
+  const [menus, setMenus] = useState([]);
+
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.menuImage)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setMenus(images);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-wrap gap-3">
+        <MenuCollection menuTitle="Menu" pages={menus.length} image={menus} />
+      </div>
+    </>
+  );
+};
+
+export default Menu;
