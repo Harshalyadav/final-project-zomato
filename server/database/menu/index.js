321c63ac8eb0be72875d5342200cc8546c33c5ed
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-10-17 12:55:48 +0530

    Create index.js

---
+++ server/database/menu/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const  MenuSchema =new mongoose.Schema({
+
+menus: [
+    {
+      name :{type : String ,required: true},
+      items :[
+          {
+              type : mongoose.Types.ObjectId ,
+              ref:"Foods"
+        },
+        
+        ],
+  
+
+},],
+
+recommended :[
+    {
+        type: mongoose.Types.ObjectId,
+        ref:"Foods",
+        unique : true,
+
+    },
+],
+
+},{
+    timestamps:true,
+});
+// Creating model of MenuSchema
+export const MenuModel = mongoose.model("Menu",MenuSchema);
