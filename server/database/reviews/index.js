0d020f1c08b89e3e8cce9549c3564be63eb4a202
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-10-17 12:58:08 +0530

    Create index.js

---
+++ server/database/reviews/index.js
@@ -0,0 +1,23 @@
+import  mongoose  from "mongoose";
+
+const ReviewSchema =mongoose.Schema({
+     
+    food:{type : mongoose.Types.ObjectId,ref:"Foods"},
+    restaurant:{type:mongoose.Types.ObjectId,ref:"Restaurants"},
+    user :{type:mongoose.Types.ObjectId,ref:"Users"},
+    rating:{type:Number,required:true},
+    reviewText:{type:String,required:true},
+    isRestaurantReview:Boolean,
+    isFoodReview:Boolean,
+    photos :[
+        {
+            type:mongoose.Types.ObjectId,
+            ref:"Images",
+        },
+    ],
+
+},{
+    timestamps:true,
+});
+
+export const ReviewModel=mongoose.model("Reviews",ReviewSchema);
