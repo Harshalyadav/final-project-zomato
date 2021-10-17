ca7eea17155d76facc2df80c385858c06c0a6e85
Harshu <54142355+Harshalyadav@users.noreply.github.com>
2021-10-17 12:38:25 +0530

    Create index.js

---
+++ server/API/Auth/index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+
+import { UserModel } from "../../database/user";
+//Validation
+
+import {ValidateSignup,ValidateSignin} from "../../Validation/auth";
+
+const Router=express.Router();
+
+Router.post("/signup",async(req, res)=>{
+    
+        try{
+           
+    await ValidateSignup(req.body.credentials);
+
+           await UserModel.findByEmailAndPhone(req.body.credentials);
+
+
+               
+             const newUser = await UserModel.create(req.body.credentials);
+
+            const token = newUser.generateJwtToken();
+            
+
+            return res.status(200).json({token ,status: "success"});
+
+
+        }catch(error){
+            return res.status(500).json({error:error.message});
+        }
+         
+});
+
+
+
+Router.post("/signin",async(req, res)=>{
+        try{
+           
+    await ValidateSignin(req.body.credentials);
+    
+           const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+
+
+            
+
+            const token = user.generateJwtToken();
+            
+
+            return res.status(200).json({token ,status: "success"});
+
+
+        }catch(error){
+            return res.status(500).json({error:error.message});
+        }
+         
+});
+
+
+Router.get("/google",passport.authenticate("google",{
+    scope: [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ],
+})
+);
+
+Router.get("/google/callback",passport.authenticate("google",{failureRedirect:"/"}),
+(req,res)=>{
+    
+    return res.redirect(
+        `http://localhost:3000/google/${req.session.passport.user.token}`);
+  });
+
+export default Router;
